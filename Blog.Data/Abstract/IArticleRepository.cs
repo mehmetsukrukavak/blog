1f9e04244228cc546d8c81531f30aed5f991ecf6
Mehmet Şükrü Kavak(Mac) <mehmetsukru.kavak07@gmail.com>
2021-07-14 11:33:15 +0300

    IArticle ve ICategory Implementasyonu

---
+++ Blog.Data/Abstract/IArticleRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Blog.Entities.Concrete;
+using Blog.Shared.Data.Abstract;
+
+namespace Blog.Data.Abstract
+{
+    public interface IArticleRepository:IEntityRepository<Article>
+    {
+
+    }
+}
